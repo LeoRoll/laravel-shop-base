d0013d941a1ec28b612aa85f65d79176398d30bf
leoroll <1455212235@qq.com>
2018-09-27 09:21:15 +0000

    触发激活邮件

---
+++ app/Notifications/EmailVerificationNotification.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Str;
+use Cache;
+
+class EmailVerificationNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    //发送邮件时会调用此方法来构建邮件内容，参数就是 App\Models\User 对象
+    public function toMail($notifiable)
+    {
+        // 使用 Laravel 内置的 Str 类生成随机字符串的函数，参数就是要生成的字符串长度
+        $token = Str::random(16);
+
+        // 往缓存中写入在这个随机的字符串，有限时间为 30 分支。
+        Cache::set('email_verification_'.$notifiable->email, $token, 30);
+        $url = route('email_verification.verify', ['email' => $notifiable->email, 'token' => $token]);
+        return (new MailMessage)
+                    ->greeting('尊敬的用户'. $notifiable->name .'您好：')
+                    ->subject('您在本商城已经注册成功，接下来请验证您的邮箱')
+                    ->line('请点击下方的链接进行验证')
+                    ->action('验证', $url);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
