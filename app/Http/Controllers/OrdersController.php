022696bb2717492e935d086d25534d4d58b8a44e
leoroll <1455212235@qq.com>
2018-11-23 07:18:43 +0000

    用户界面 - 使用优惠券下单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -13,6 +13,8 @@ use App\Services\CartService;
 use App\Services\OrderService;
 use App\Http\Requests\SendReviewRequest;
 use App\Http\Requests\ApplyRefundRequest;
+use App\Exceptions\CouponCodeUnavailableException;
+use App\Models\CouponCode;
 
 class OrdersController extends Controller
 {
@@ -39,6 +41,8 @@ class OrdersController extends Controller
     {
     	$user = $request->user();
         $address = UserAddress::find($request->input('address_id'));
+
+        $coupon  = null;
     	// // 开启一个数据库事务，并且把 $cartService 加入 use 中
     	// $order = \DB::transaction(function()use($user, $request, $cartService){
     	// 	$address = UserAddress::find($request->input('address_id'));
@@ -98,7 +102,17 @@ class OrdersController extends Controller
      //    $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
 
     	// return $order;
-        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
+
+        // 如果用户提交了优惠码
+        if ($code = $request->input('coupon_code')) {
+            $coupon = CouponCode::where('code', $code)->first();
+            if (!$coupon) {
+                throw new CouponCodeUnavailableException('优惠券不存在');
+            }
+        }
+
+        // 参数中加入 $coupon 变量
+        return $orderService->store($user, $address, $request->input('remark'), $request->input('items'), $coupon);
     }
 
     // 收货
